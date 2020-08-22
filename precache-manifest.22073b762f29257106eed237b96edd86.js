f9fec0f18978ce0f37ccce75f86fa4bc4b83deac
andres <aebb001@gmail.com>
2020-08-22 10:46:39 -0400

    Updates

---
+++ precache-manifest.22073b762f29257106eed237b96edd86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf611de024f137533f51e48dfac4cea",
+    "url": "/flags/index.html"
+  },
+  {
+    "revision": "6dca8d390a5e91cc2678",
+    "url": "/flags/static/css/main.08a1540f.chunk.css"
+  },
+  {
+    "revision": "4b6cb027586882903e19",
+    "url": "/flags/static/js/2.e3daecfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flags/static/js/2.e3daecfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dca8d390a5e91cc2678",
+    "url": "/flags/static/js/main.33417293.chunk.js"
+  },
+  {
+    "revision": "c8523fa0f32a07ffba75",
+    "url": "/flags/static/js/runtime-main.59ad6603.js"
+  }
+]);
